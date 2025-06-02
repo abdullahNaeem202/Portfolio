bafef4514bdedebd1e94fb5b9bb88371b0b81c7c
abdu <ana105@sfu.ca>
2025-06-02 14:41:15 -0700

    Added Header component with intro and links

---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+
+function Header() {
+    return(
+        <header>
+            <h3>Hi! My name is</h3>
+            <h1>Abdullah Naeem</h1>
+            <p> I am a Data Science graduate from Simon Fraser University with a passion in data analysis and 
+                transforming data into actionable business insights.</p>
+            <div>
+                <nav>
+                    <ul>  
+                        <li> <a href = "https://github.com/Hav0"> GitHub</a></li>
+                        <li> <a href = "https://www.linkedin.com/in/naeemabdullah2002/"> LinkedIn</a> </li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+    );
+}
+
+export default Header
