fe36c8139ffe61c189bf28d34a7a73af7b0fbef7
abdu <ana105@sfu.ca>
2025-06-30 23:42:32 -0700

    Updated Toggle and added responsiveness for mobile

--- src/components/Toggle.jsx
+++ src/components/Toggle.jsx
@@ -1,13 +1,11 @@
-import { useState } from "react"; /* hook - to implement button state - so we can switch states from dark to light mode*/
 import { useEffect } from "react"; /* hook - to set the dark and light mode colour effects on screen */
-import { faSun } from "@fortawesome/free-solid-svg-icons";
 import { faMoon } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import useLocalStorage from "use-local-storage"; // importing local storage to keep track of user choice for dark/light mode
 
-function Toggle() {
+function Toggle({isGlobal}) { // isGlobal is a react prop that I can change the value of inside this component
     const user_preference = window.matchMedia("(prefers-color-scheme: dark)").matches;
-    const [darkMode, setDarkMode] = useLocalStorage('darkMode', user_preference); /* Initializing useState hook */
+    const [darkMode, setDarkMode] = useLocalStorage('darkMode', user_preference); /* keeping track of user state */
 
     /* our effect function */
     useEffect(() => {
@@ -19,10 +17,12 @@ function Toggle() {
     }
 
     return(
-    <div className="absolute top-7 left-7">
+    <div className={isGlobal ? "md:flex": "hidden"}>
         <button className="w-16 h-16 bg-[#7c828c] dark:bg-[#f6f7f2] rounded-full font-semibold" onClick = {toggleDarkMode}  >
         {darkMode ? 
-        <FontAwesomeIcon icon = {faSun} size = '2x' aria-label = 'LightMode'/> : <FontAwesomeIcon icon = {faMoon} size = '2x' aria-label = 'DarkMode' /> }
+        <svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 30 30" width="40px" height="40px" className="mx-auto" aria-label="LightMode">    
+        <path d="M 14.984375 0.98632812 A 1.0001 1.0001 0 0 0 14 2 L 14 5 A 1.0001 1.0001 0 1 0 16 5 L 16 2 A 1.0001 1.0001 0 0 0 14.984375 0.98632812 z M 5.796875 4.7988281 A 1.0001 1.0001 0 0 0 5.1015625 6.515625 L 7.2226562 8.6367188 A 1.0001 1.0001 0 1 0 8.6367188 7.2226562 L 6.515625 5.1015625 A 1.0001 1.0001 0 0 0 5.796875 4.7988281 z M 24.171875 4.7988281 A 1.0001 1.0001 0 0 0 23.484375 5.1015625 L 21.363281 7.2226562 A 1.0001 1.0001 0 1 0 22.777344 8.6367188 L 24.898438 6.515625 A 1.0001 1.0001 0 0 0 24.171875 4.7988281 z M 15 8 A 7 7 0 0 0 8 15 A 7 7 0 0 0 15 22 A 7 7 0 0 0 22 15 A 7 7 0 0 0 15 8 z M 2 14 A 1.0001 1.0001 0 1 0 2 16 L 5 16 A 1.0001 1.0001 0 1 0 5 14 L 2 14 z M 25 14 A 1.0001 1.0001 0 1 0 25 16 L 28 16 A 1.0001 1.0001 0 1 0 28 14 L 25 14 z M 7.9101562 21.060547 A 1.0001 1.0001 0 0 0 7.2226562 21.363281 L 5.1015625 23.484375 A 1.0001 1.0001 0 1 0 6.515625 24.898438 L 8.6367188 22.777344 A 1.0001 1.0001 0 0 0 7.9101562 21.060547 z M 22.060547 21.060547 A 1.0001 1.0001 0 0 0 21.363281 22.777344 L 23.484375 24.898438 A 1.0001 1.0001 0 1 0 24.898438 23.484375 L 22.777344 21.363281 A 1.0001 1.0001 0 0 0 22.060547 21.060547 z M 14.984375 23.986328 A 1.0001 1.0001 0 0 0 14 25 L 14 28 A 1.0001 1.0001 0 1 0 16 28 L 16 25 A 1.0001 1.0001 0 0 0 14.984375 23.986328 z"/></svg>:
+         <FontAwesomeIcon icon = {faMoon} size = '2x' aria-label = 'DarkMode' /> }
         </button>
     </div>
     )
