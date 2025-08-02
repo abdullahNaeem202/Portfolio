8561edf548534998e8899b762dd90f37263df33f
abdu <ana105@sfu.ca>
2025-08-01 20:31:46 -0700

    Project previews and updated App component

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,21 @@ import Header from './components/Header.jsx'
 import NavigationBar from './components/NavigationBar.jsx'
 import About from './components/About.jsx'
 import Experience from './components/Experience.jsx';
+import Projects from './components/Projects.jsx';
+import Contact from './components/Contact.jsx';
 
 function App() {
     return (
       <>
+      <div className='overflow-x-hidden'>
         <NavigationBar></NavigationBar>
         <Header></Header>
         <Toggle></Toggle>
         <About></About>
         <Experience></Experience>
-
+        <Projects></Projects>
+        <Contact></Contact>
+      </div>
 
       </>
 
